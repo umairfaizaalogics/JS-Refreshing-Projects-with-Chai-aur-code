51ab2eb27181e83234b6fe0a77903ad367bc3a60
umairfaizaalogics <aalogics.umair@gmail.com>
2024-06-03 12:36:34 +0000

    Datatypes

---
+++ JS_Basics/Datatypes.js
@@ -0,0 +1,17 @@
+"use strick"; // treat all JS code as a newer version.
+
+// alert(3+3); // We are using nodejs, not browser.. 
+
+// Ecma script define JS Standards.
+
+// Primitives Datatypes
+
+let name = "Umair Faiz"  // String
+let age = 23    // integer 
+let isLoggedIn = true // boolean.
+null;  // undefined/Standalone value./ Representation of empty value.
+// symbols.
+
+// Object.
+console.log(typeof null);  //Null is a Object.
+console.log(typeof undefined); // undefined.
