72c7ec91785c5b0a078424ce2248327b568870e2
umairfaizaalogics <aalogics.umair@gmail.com>
2024-06-03 11:16:41 +0000

    Variables

---
+++ JS_Basics/Variables.js
@@ -0,0 +1,16 @@
+const accountID = 133556;
+let accountEmail = "umair@gmail.com";
+var accountPassword = "1234567";
+acccountCity = "Karachi";
+let accountState;
+
+// accountID = 2; // Not allowd, Because this is under in Const keyword.!
+
+accountEmail = "aaf@gmail.com"
+accountPassword = "24356234"
+acccountCity = "hyderabad"
+
+console.table([accountEmail, accountID, accountPassword, acccountCity, accountState])
+
+// Prefer not to use var 
+// Because o issue in block scope and functional scope.
