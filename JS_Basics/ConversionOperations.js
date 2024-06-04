89e9240716f9977790a487d30ad15823da4a084c
umairfaizaalogics <aalogics.umair@gmail.com>
2024-06-04 11:18:12 +0000

    Coversions of Dataypes

---
+++ JS_Basics/ConversionOperations.js
@@ -0,0 +1,25 @@
+// let score = "Umair"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33cdv" => NaN (Not a Number)
+// true => 1; false = 0;
+ 
+// let isLoggedIn = "Umair";
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Umair" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber) 
+console.log(typeof stringNumber);
