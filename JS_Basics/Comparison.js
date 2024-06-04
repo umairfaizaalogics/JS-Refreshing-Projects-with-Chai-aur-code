112d709a6d3caaeccdde52f733b0627be6b5d387
umairfaizaalogics <aalogics.umair@gmail.com>
2024-06-04 14:08:45 +0000

    comparisons

---
+++ JS_Basics/Comparison.js
@@ -0,0 +1,14 @@
+/**Comparison */ 
+// console.log(2 > 1); True or False
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // yhn pr Value conversion ajata hai isleye ye True dey rha hai.
